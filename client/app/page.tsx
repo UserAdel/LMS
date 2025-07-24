bcd8339ac41d408e45fd6222af6d23643a9bbfc7
adel hany <adel58420@gmail.com>
2025-07-25 01:04:59 +0300

    Building Login Page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,11 @@
+import { ThemeToggle } from "@/components/themeToggle";
 import Image from "next/image";
 
 export default function Home() {
 return(
   <div>
-    <h1 className="text-3xl font-bold underline text-red-500">Hello World</h1>
+    <h1 className="text-3xl font-bold underline text-red-500 ">Home Page</h1>
+    <ThemeToggle/>
   </div>
 );
 }
