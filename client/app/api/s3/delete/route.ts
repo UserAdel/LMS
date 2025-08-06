8caf65ff9e88ca3a8f60838ef0be94a4c8c3a454
adel hany <adel58420@gmail.com>
2025-08-06 21:25:00 +0300

    Arcjet integration and apply rate limit and detectBotion to DropZone routes

--- client/app/api/s3/delete/route.ts
+++ client/app/api/s3/delete/route.ts
@@ -2,9 +2,36 @@ import { env } from "@/lib/env";
 import { DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { NextResponse } from "next/server";
 import { S3 } from "@/lib/S3Client";
+import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
 
+const aj = arcjet
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: [],
+    })
+  )
+  .withRule(
+    fixedWindow({
+      mode: "LIVE",
+      window: "1m",
+      max: 5,
+    })
+  );
 export async function DELETE(Request: Request) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
   try {
+    const decision = await aj.protect(Request, {
+      fingerprint: session?.user.id as string,
+    });
+    if (decision.isDenied()) {
+      return NextResponse.json({ error: "Blocked by Arcjet" }, { status: 403 });
+    }
     const body = await Request.json();
     const { key } = body;
     if (!key) {
--- client/app/api/s3/upload/route.ts
+++ client/app/api/s3/upload/route.ts
@@ -5,6 +5,9 @@ import { z } from "zod";
 import { v4 as uuidv4 } from "uuid";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { S3 } from "@/lib/S3Client";
+import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
 export const fileUploadSchema = z.object({
   fileName: z.string().min(1, { message: "FileName is required" }),
@@ -13,8 +16,32 @@ export const fileUploadSchema = z.object({
   isImage: z.boolean(),
 });
 
+const aj = arcjet
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: [],
+    })
+  )
+  .withRule(
+    fixedWindow({
+      mode: "LIVE",
+      window: "1m",
+      max: 5,
+    })
+  );
+
 export async function POST(Request: Request) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
   try {
+    const decision = await aj.protect(Request, {
+      fingerprint: session?.user.id as string,
+    });
+    if (decision.isDenied()) {
+      return NextResponse.json({ error: "Blocked by Arcjet" }, { status: 403 });
+    }
     const body = await Request.json();
     const validation = fileUploadSchema.safeParse(body);
     if (!validation.success) {
