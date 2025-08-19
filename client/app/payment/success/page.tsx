f49d7aaa992f18b8e21998bc7693b7ce3003724a
adel hany <adel58420@gmail.com>
2025-08-19 23:39:18 +0300

    Payment Success and cancel Pages

--- client/app/payment/success/page.tsx
+++ client/app/payment/success/page.tsx
@@ -1,27 +1,35 @@
+"use client";
 import { buttonVariants } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import { ArrowLeft, XIcon } from "lucide-react";
+import { useConfetti } from "@/hooks/use-confetti";
+import { ArrowLeft, CheckIcon } from "lucide-react";
 import Link from "next/link";
+import { useEffect } from "react";
 
-export default function PaymentCancelled() {
+export default function PaymentSuccess() {
+  const { triggerConfetti } = useConfetti();
+  useEffect(() => {
+    triggerConfetti();
+  }, []);
   return (
     <div className="w-full min-h-screen flex flex-1 justify-center items-center">
       <Card className="w-[350px]">
         <CardContent>
           <div className=" w-full flex justify-center">
-            <XIcon className="size-12 p-2 bg-red-500/30 text-red-500 rounded-full" />
+            <CheckIcon className="size-12 p-2 bg-green-500/30 text-green-500 rounded-full" />
           </div>
           <div className="mt-3 text-center sm:mt-5 w-full">
-            <h2 className="text-xl font-semibold ">Payment Cancelled</h2>
+            <h2 className="text-xl font-semibold ">Payment Success</h2>
             <p className="text-sm mt-2 text-muted-foreground tracking-tight text-balance">
-              No worries, you won't be charged. Please try again later
+              congrate you payment was successful. you should now have access to
+              the course
             </p>
             <Link
               className={buttonVariants({ className: "w-full mt-5" })}
-              href="/"
+              href="/dashboard"
             >
               <ArrowLeft />
-              Go Back To HomePage{" "}
+              Go Back To Dashboard{" "}
             </Link>
           </div>
         </CardContent>
