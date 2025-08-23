9610b7c1c2dee33ae37869498abb324a3fa02848
adel hany <adel58420@gmail.com>
2025-08-24 02:50:13 +0300

    force-dynamic for Course Page

--- client/app/admin/courses/page.tsx
+++ client/app/admin/courses/page.tsx
@@ -8,6 +8,7 @@ import {
 import { EmptyState } from "@/components/general/EmptyState";
 import { Suspense } from "react";
 
+export const dynamic = "force-dynamic";
 export default async function CoursesPage() {
   return (
     <>
