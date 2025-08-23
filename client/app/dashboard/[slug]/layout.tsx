3511c77e7980022bc4fc6b215750421490493ef9
adel hany <adel58420@gmail.com>
2025-08-23 03:26:23 +0300

    feat:Single Course Page UI

--- client/app/dashboard/[slug]/layout.tsx
+++ client/app/dashboard/[slug]/layout.tsx
@@ -1,13 +1,19 @@
-export default function CourseLayout({
-  children,
-}: {
+import { getCourseSidebarData } from "@/app/data/course/get-course-sidebar-data";
+import { CourseSidebar } from "../_components/CourseSidebar";
+
+interface iAppProps {
+  params: Promise<{ slug: string }>;
   children: React.ReactNode;
-}) {
+}
+export default async function CourseLayout({ children, params }: iAppProps) {
+  const { slug } = await params;
+  const course = await getCourseSidebarData(slug);
+
   return (
     <div className="flex flex-1">
       {/* siderbar-30% */}
       <div className="w-80 border-r border-border shrink-0 ">
-        <h1>SideBar</h1>
+        <CourseSidebar course={course.course} />
       </div>
       {/* main-content-70% */}
       <div className="flex-1 overflow-hidden">{children}</div>
--- client/app/dashboard/[slug]/page.tsx
+++ client/app/dashboard/[slug]/page.tsx
@@ -1,5 +1,3 @@
-type Params = Promise<{ slug: string }>;
-export default async function CourseSlugRoute({ params }: { params: Params }) {
-  const { slug } = await params;
-  return <h1>hello world {slug}</h1>;
+export default async function CourseSlugRoute() {
+  return <h1>hello world </h1>;
 }
