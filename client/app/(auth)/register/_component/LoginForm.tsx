bbe1911e5fc726f7db871e85d9e1c568025bf67c
adel hany <adel58420@gmail.com>
2025-08-26 18:53:39 +0300

    debugging

--- client/app/(auth)/register/_component/LoginForm.tsx
+++ client/app/(auth)/register/_component/LoginForm.tsx
@@ -65,7 +65,7 @@ export function LoginForm() {
         email: email, // required
         name: "user",
         password: password,
-        callbackURL: "https://lms-azure-tau.vercel.app/",
+        // callbackURL: "https://lms-azure-tau.vercel.app/",
         fetchOptions: {
           onSuccess: () => {
             toast.success("Email Sent");
