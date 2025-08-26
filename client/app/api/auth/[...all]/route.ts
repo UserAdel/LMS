f1cd26883e06905601116fb9358278d87ddb846c
adel hany <adel58420@gmail.com>
2025-08-26 18:26:10 +0300

    another

--- client/app/api/auth/[...all]/route.ts
+++ client/app/api/auth/[...all]/route.ts
@@ -30,7 +30,7 @@ const botOptions = {
 const rateLimitOptions = {
   mode: "LIVE",
   interval: "2m", // counts requests over a 2 minute sliding window
-  max: 5, // allows 5 submissions within the window
+  max: 50, // allows 5 submissions within the window
 } satisfies SlidingWindowRateLimitOptions<[]>;
 
 const signupOptions = {
